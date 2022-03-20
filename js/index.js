a5cb09deaa2f9981024fd8fd5526a564f9658489
joaoalsousa <joaoantoniolopessousa@gmail.com>
2022-03-20 15:23:49 +0000

    Fixed javascript target blank console error showing

--- js/index.js
+++ js/index.js
@@ -114,10 +114,12 @@ $('#page-top-btn').click(function(){
 /* Smooth scroll */
 
 $('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 750);
-    return false;
+    if($(this).attr('target') != '_blank'){
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 750);
+        return false;
+    }
 });
 
 /* Smooth scroll */
